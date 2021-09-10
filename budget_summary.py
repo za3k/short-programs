c11a0d48b63a1f9c3c48180663e83da3329077af
Zachary Vance <za3k@za3k.com>
2021-09-10 15:11:30 -0700

    Helper for weekly review

--- budget_summary.py
+++ budget_summary.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-import datetime, os, re, sys
+import datetime, glob, os, re, sys
 
 if len(sys.argv) > 1:
     fp = sys.argv[1]
@@ -24,14 +24,40 @@ lines = []
 for line in budget[2:]:
     date, place, amount, category, thing = [x.strip() for x in line.split("|")[1:]]
     lines.append((float(amount), category))
-print("{: <12} {:.2f}".format("Total:", sum(amount for (amount, category) in lines)))
-print("{: <12} {:.2f}".format("Total (no rent):", sum(amount for (amount, category) in lines if category != "Rent")))
-categories = sorted(set(category for (amount, category) in lines))
-print()
-OTHER = ("Grocery", "Luxury", "Good", "Restaurant")
-for category in categories:
-    if category not in OTHER:
-        print("{: <12} {:.2f}".format(category+":", sum(amount for (amount, c) in lines if category == c)))
-print("{: <12} {:.2f}".format("Other"+":", sum(amount for (amount, c) in lines if c in OTHER)))
-for category in OTHER:
-    print("  {: <12} {:.2f}".format(category+":", sum(amount for (amount, c) in lines if category == c)))
+
+if lines == []: # Budget not done yet
+    log_book = glob.glob("/home/zachary/physicalish_documents/log-book*CURRENT*")[0]
+    with open(log_book, "r") as f:
+        lines = list(line for line in f)
+    finance_page = re.compile('--page (?P<page>[1-9][0-9]*), finances')
+    start_line, end_line = None, None
+    for i, line in enumerate(lines):
+        if finance_page.match(line):
+            if start_line is None:
+                start_line = i
+            else:
+                end_line = i
+                break
+    total = 0
+    for line in lines[start_line+1:end_line]:
+        date, place, amount, hand_total, thing = line[:10], line[28:48].strip(), line[48:57].strip(), line[57:65].strip(), line[66:].strip()
+        total += float(amount)
+        hand_total = float(hand_total)
+        print("| {date:<11}| {place:<14}|{amount:>11} | {category:<14}| {thing}".format(date=date, place=place, amount=amount, category="", thing=thing))
+    print()
+    if (total - hand_total) < 0.005:
+        print("Total was correct")
+    else:
+        print("Correct total should be: {}".format(total))
+else: # Budget is done yet
+    print("{: <12} {:.2f}".format("Total:", sum(amount for (amount, category) in lines)))
+    print("{: <12} {:.2f}".format("Total (no rent):", sum(amount for (amount, category) in lines if category != "Rent")))
+    categories = sorted(set(category for (amount, category) in lines))
+    print()
+    OTHER = ("Grocery", "Luxury", "Good", "Restaurant")
+    for category in categories:
+        if category not in OTHER:
+            print("{: <12} {:.2f}".format(category+":", sum(amount for (amount, c) in lines if category == c)))
+    print("{: <12} {:.2f}".format("Other"+":", sum(amount for (amount, c) in lines if c in OTHER)))
+    for category in OTHER:
+        print("  {: <12} {:.2f}".format(category+":", sum(amount for (amount, c) in lines if category == c)))
